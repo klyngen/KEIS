0c62ece49aaabc7233353aebb22a3278d1618fae
Martin Klingenberg <martkli@stud.ntnu.no>
2018-03-02 22:31:41 +0100

    various UX-details as well as spawning some elements on-demand. This solves the issue of overloading the backend

--- app/Rent.php
+++ app/Rent.php
@@ -7,4 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 class Rent extends Model
 {
     protected $fillable = ['users', 'instances', 'stop'];
+    protected $appends = ['equipment', 'instance', 'user'];
+
+
+    protected function getEquipmentAttribute() {
+        return Equipment::where('id', $this->instance()->attributes['equipment'])->first();
+    }
+
+
+    protected function getInstanceAttribute() {
+        return $this->instance();
+    }
+
+    private function instance() {
+        $instance = Instance::where('id', $this->attributes['instances'])->first();
+        return $instance;
+    }
+
+    protected function getUserAttribute() {
+        return User::where('id', $this->attributes['users'])->first();
+    }
+
 }
